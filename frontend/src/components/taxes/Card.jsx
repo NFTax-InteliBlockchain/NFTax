dc7e2c9ad3815d23948c4087e800f9388548f55c
elisaflemer <elisaflemer@gmail.com>
2023-04-07 20:51:59 -0300

    feat(Cards): cards receiving props

--- frontend/src/components/taxes/Card.jsx
+++ frontend/src/components/taxes/Card.jsx
@@ -1,16 +1,20 @@
-export function Card() {
+export function Card(props) {
+
+    const monthNames = ["Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Septembro", "Outubro", "Novembro", "Dezembro"];
+
     return (
-        <div className="w-full h-20 bg-[#fffefe] shadow-md flex items-center justify-between">
-            <div className="bg-green-500 w-1 h-full"></div>
-            <div className="flex flex-col bg-white rounded-2 items-center font-bold shadow-sm py-2">
-                <p>12</p>
-                <p>Dez</p>
+        <div className="w-full h-20 bg-[#fffefe] shadow-md flex items-center  gap-5 relative">
+
+            <div className={props.paid ? "bg-green-500 w-1 h-full absolute left-0" : "bg-red-500 w-1 h-full absolute left-0"} ></div>
+            <div className="flex flex-col bg-white rounded-2 items-center font-bold shadow-sm py-2 pl-5">
+                <p>{new Date(Date.parse(props.date)).getDay()}</p>
+                <p>{monthNames[new Date(Date.parse(props.date)).getMonth()].substring(0, 3)}/{String(new Date(Date.parse(props.date)).getFullYear()).substring(2, 4)}</p>
             </div>
-            <div className="flex flex-col items-start justify-start py-2">
-                <p>BRKM5</p>
-                <p className="text-sm">Braskem SA</p>
+            <div className="flex flex-col items-start justify-start py-2 w-40 pl-2">
+                <p>{props.abbreviation}</p>
+                <p className="text-sm">{props.name}</p>
             </div>
-            <p className="pr-8">R$ 30.000,00</p>
+            <p className="pr-8">R$ {props.taxes}</p>
         </div>
     )
 }
--- frontend/src/pages/Taxes.jsx
+++ frontend/src/pages/Taxes.jsx
@@ -5,41 +5,59 @@ import { BsArrowLeft } from 'react-icons/bs'
 import { BsArrowRight } from 'react-icons/bs'
 import { Card } from '../components/taxes/Card'
 
+import { cards } from './data'
 
 export function Taxes() {
-
     const [totalTaxes, setTotalTaxes] = useState(0)
+
+    const [selectedCard, setSelectedCard] = useState(null)
+
+    const [selectedType, setSelectedType] = useState('all')
 
     return (
         <div className="bg-blue h-full font-montserrat w-full">
             <div className='p-4 flex flex-col justify-center items-center w-full'>
                 <BsArrowLeft className='text-white text-xl absolute left-4 top-5' />
-                <div className='flex flex-col pt-44 justify-center items-center gap-2'>
+                <div className='flex flex-col pt-36 justify-center items-center gap-2'>
                     <h2 className='font-bold text-white text-xl'>Selecione o período desejado</h2>
                     <Date />
                 </div>
 
             </div>
             <div className='bg-darkblue text-white flex gap-4 px-1 py-3 w-full mt-20 justify-center'>
-                <button>Todas as operações</button>
-                <button>Renda fixa</button>
-                <button>Renda variável</button>
+                <button onClick={() => setSelectedType('all')}>Todas as operações</button>
+                <button onClick={() => setSelectedType('fixed')}>Renda fixa</button>
+                <button onClick={() => setSelectedType('variable')}> Renda variável</button>
             </div>
             <div className='bg-white text-black justify-center flex gap-1 py-5 w-full px-1'>Total de impostos:<p className='font-bold'>R$ {totalTaxes}</p></div>
             <div className='h-full bg-grey'>
                 <div className='mx-3 py-4 flex flex-col gap-3'>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                    {cards.map((card) => {
+                        console.log(card)
+                        return (
+                            <Card
+                                key={card.id}
+                                abbreviation={card.abbreviation}
+                                name={card.name}
+                                value={card.value}
+                                taxes={card.taxes}
+                                date={card.date}
+                                type={card.type}
+                                paid={card.paid}
+                            />
+                        )
+                    })}
                 </div>
-                
+
             </div>
             <div className="bg-darkblue text-white px-1 py-3 w-full bottom-0 flex justify-center items-center font-bold sticky">
                 <span className="text-center">Pagar tudo</span>
                 <BsArrowRight className="text-xl absolute right-3 text-lightblue" />
             </div>
+
+            {/* <div className='fixed bottom-0 h-[40%] w-full bg-white rounded-t-3xl shadow-2xl'>
+
+            </div> */}
         </div>
     )
 }
