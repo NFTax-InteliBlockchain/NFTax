98847622d889df5673d47165e863a3d6c2987f82
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-04-07 20:26:43 -0300

    feat(connect metamask): 'connect' page can access metamask extension

--- frontend/src/pages/Connect.jsx
+++ frontend/src/pages/Connect.jsx
@@ -1,7 +1,24 @@
 import { Button } from '../components/taxes/Button'
-
+import { useState } from 'react';
 
 export function Connect() {
+
+    const [walletAddres, setWalletAddres] = useState('');
+    async function requestAccout(){
+        if(window.ethereum){
+            try{
+                const accounts = await window.ethereum.request({
+                    method: 'eth_requestAccounts'
+                });
+                setWalletAddres("Conectado a carteira" + " " + accounts[0]);
+            } catch (error) {
+                console.log(error);
+            }
+        }else{
+            alert("Instale metamask em seu navegador!")
+        }
+    }
+
     return (
         <div className="bg-darkblue h-screen ">
             <div className="max-w-s pl-6 pt-6 mx-auto pb-44">
@@ -10,11 +27,15 @@ export function Connect() {
                 <h2 className="font-montserrat font-medium pt-4 text-slate-50">NFTax</h2>
                 <h2 className="font-montserrat font-bold text-slate-50">Sua vida fiscal simplificada</h2>
                 <h3  className="text-slate-50 pt-16 pb-6">Conecte os seus dados do Real Digital</h3>
-                <Button text="Conectar" />
+                {/* <Button text="Conectar" /> */}
+                <button className="bg-button-darkblue text-slate-50 pt-1 pb-1 pl-8 pr-8 rounded-md hover:bg-sky-700 transform transition hover:scale-110"
+                onClick={requestAccout}
+                 >Conectar carteita</button>
+                <h3 className='text-slate-50 text-xs pt-4'>{walletAddres}</h3>
             
             </div>   
             <div className='bg-white pl-6 pb-16 rounded-lg'>
-                    <h2 className="font-montserrat font-bold pt-4 pb-4">Por que fazer isso?</h2>
+                    <h2 className="font-montserrat font-bold pt-4 pb-8">Por que fazer isso?</h2>
                     <p> • Sem dor de cabeça com imposto de renda</p>
                     <p> • Pagamento facilitado em blockchain</p>
                     <p> • Geração de relatórios</p>
