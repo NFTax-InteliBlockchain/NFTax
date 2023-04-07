e177be793420fb8478fb2efd37c980d72e60422a
elisaflemer <elisaflemer@gmail.com>
2023-04-07 18:59:47 -0300

    feat(Taxes): added cards component

--- frontend/src/components/taxes/Card.jsx
+++ frontend/src/components/taxes/Card.jsx
@@ -1,7 +1,16 @@
 export function Card() {
     return (
-        <div className="w-[80%] mx-5">
-            CARD
+        <div className="w-full h-20 bg-[#fffefe] shadow-md flex items-center justify-between">
+            <div className="bg-green-500 w-1 h-full"></div>
+            <div className="flex flex-col bg-white rounded-2 items-center font-bold shadow-sm py-2">
+                <p>12</p>
+                <p>Dez</p>
+            </div>
+            <div className="flex flex-col items-start justify-start py-2">
+                <p>BRKM5</p>
+                <p className="text-sm">Braskem SA</p>
+            </div>
+            <p className="pr-8">R$ 30.000,00</p>
         </div>
     )
 }
--- frontend/src/pages/Taxes.jsx
+++ frontend/src/pages/Taxes.jsx
@@ -11,25 +11,32 @@ export function Taxes() {
     const [totalTaxes, setTotalTaxes] = useState(0)
 
     return (
-        <div className="bg-blue h-screen font-montserrat w-full overflow-hidden">
+        <div className="bg-blue h-full font-montserrat w-full">
             <div className='p-4 flex flex-col justify-center items-center w-full'>
                 <BsArrowLeft className='text-white text-xl absolute left-4 top-5' />
-                <div className='flex flex-col pt-40 justify-center items-center gap-2'>
+                <div className='flex flex-col pt-44 justify-center items-center gap-2'>
                     <h2 className='font-bold text-white text-xl'>Selecione o período desejado</h2>
                     <Date />
                 </div>
 
             </div>
-            <div className='bg-darkblue text-white flex gap-4 px-1 py-3 w-full mt-20'>
+            <div className='bg-darkblue text-white flex gap-4 px-1 py-3 w-full mt-20 justify-center'>
                 <button>Todas as operações</button>
                 <button>Renda fixa</button>
                 <button>Renda variável</button>
             </div>
             <div className='bg-white text-black justify-center flex gap-1 py-5 w-full px-1'>Total de impostos:<p className='font-bold'>R$ {totalTaxes}</p></div>
             <div className='h-full bg-grey'>
+                <div className='mx-3 py-4 flex flex-col gap-3'>
                 <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                </div>
+                
             </div>
-            <div className="bg-darkblue text-white px-1 py-3 w-full absolute bottom-0 flex justify-center items-center font-bold">
+            <div className="bg-darkblue text-white px-1 py-3 w-full bottom-0 flex justify-center items-center font-bold sticky">
                 <span className="text-center">Pagar tudo</span>
                 <BsArrowRight className="text-xl absolute right-3 text-lightblue" />
             </div>
