a6ffffa67e73940f2e0b3a3de896a8396b268278
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-04-07 17:21:08 -0300

    feat(pag connect): pag connect done

--- frontend/src/pages/Connect.jsx
+++ frontend/src/pages/Connect.jsx
@@ -4,8 +4,8 @@ import { Button } from '../components/taxes/Button'
 export function Connect() {
     return (
         <div className="bg-darkblue h-screen ">
-            <div className="max-w-s pl-6 pt-6 mx-auto pb-12 ">
-                <img src='./arrow.svg' className=""></img>
+            <div className="max-w-s pl-6 pt-6 mx-auto pb-44">
+                <button><img src='./arrow.svg' className="transform transition hover:scale-110"></img></button>
                 <img src='./money.svg' className="pt-36"></img>
                 <h2 className="font-montserrat font-medium pt-4 text-slate-50">NFTax</h2>
                 <h2 className="font-montserrat font-bold text-slate-50">Sua vida fiscal simplificada</h2>
@@ -13,12 +13,12 @@ export function Connect() {
                 <Button text="Conectar" />
             
             </div>   
-            <div className='bg-white max-w-s pl-6 w-1/4'>
-                    <h2 className="font-montserrat font-bold pt-4 ">Por que fazer isso?</h2>
+            <div className='bg-white pl-6 pb-16 rounded-lg'>
+                    <h2 className="font-montserrat font-bold pt-4 pb-4">Por que fazer isso?</h2>
                     <p> • Sem dor de cabeça com imposto de renda</p>
                     <p> • Pagamento facilitado em blockchain</p>
                     <p> • Geração de relatórios</p>
-                </div>   
+            </div>   
         </div>
     )
 }
