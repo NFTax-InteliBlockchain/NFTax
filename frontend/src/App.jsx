18334f1e8e6c2093b53309efaecdbc30d22e1fea
elisaflemer <elisaflemer@gmail.com>
2023-04-07 15:40:00 -0300

    feat(pages): add routes to main pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Taxes } from './pages/Taxes'
+import { Home } from "./pages/Home";
+import { Connect } from "./pages/Connect";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/taxes" element={<Taxes/>}>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/taxes" element={<Taxes />}>
+        </Route>
+        <Route path="/connect" element={<Connect />}>
         </Route>
       </Routes>
     </Router>
---
+++ frontend/src/pages/Connect.jsx
@@ -0,0 +1,7 @@
+export function Connect() {
+    return (
+        <div className="bg-darkblue h-screen">
+            CONNECT
+        </div>
+    )
+}
